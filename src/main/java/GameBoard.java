d5f9513d939e60814f468abac0b8494f0dd6b730
bdietz56 <bradleydietz@Bradleys-MacBook-Pro.local>
2017-10-25 13:57:21 -0400

    Set up Development

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -2,11 +2,12 @@ import java.util.*;
 import java.text.*;
 import java.io.*;
 
-public class WorldOfSweets
+public class GameBoard
 {
   public static void main(String[] args) {
     System.out.println("hey");
-    GameBoard g = new GameBoard();
+
+
 
   }
 

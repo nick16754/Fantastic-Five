787a4fffe48f17280584eb7ad32a84041931fa65
ken woodruff <kjhw9979@gmail.com>
2017-10-25 14:24:53 -0400

    Formatted code

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -6,72 +6,75 @@ import java.util.*;
 import java.text.*;
 import java.io.*;
 
-public class GameBoard
-{
+public class GameBoard {
 
-  static JFrame _frame = new JFrame("World of Sweets");
+    static JFrame _frame = new JFrame("World of Sweets");
 
-  public GameBoard() {
-    create_board();
-  }
-
-  public Boolean create_board()
-  {
-    // Center the frame
-    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-    int x = (int) ((dimension.getWidth() - _frame.getWidth()) / 2);
-    int y = (int) ((dimension.getHeight() - _frame.getHeight()) / 2);
-    _frame.setLocation(x, y);
-
-    _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    _frame.setSize(1000, 1000);
-
-
-    JPanel MainPanel = new JPanel();
-
-    _frame.add(MainPanel);
-
-    GridLayout experimentLayout = new GridLayout(10,10);
-    MainPanel.setLayout(experimentLayout);
-
-    JPanel[][] squares = new JPanel[10][10] ;
-
-    int rowCounter = 0;
-    int rows = 0;
-
-    for (int i = 0; i < 100; i++) {
-
-      JPanel sub = new JPanel();
-//            sub.setBorder(BorderFactory.createLineBorder(Color.black));
-//            sub.setBackground(Color.red);
-      sub.setSize(100, 100);
-      sub.addMouseListener(new MouseAdapter() {
-        @Override
-        public void mousePressed(MouseEvent e) {
-          System.out.println("Click");
-        }
-      });
-
-
-      if (rowCounter >= 10) {
-        rowCounter = 0;
-        rows++;
-      }
-
-      System.out.println("Adding square at row " + rows + ", col " + (i%10));
-      squares[rows][i%10] = sub;
-
-
-      MainPanel.add(sub);
-
-      rowCounter++;
+    public GameBoard() {
+        create_board();
+        initialize();
     }
 
-    squares[1][1].setBackground(Color.green);
+    private void initialize() {
+        
+    }
 
-    _frame.setVisible(true);
+    public Boolean create_board() {
+        // Center the frame
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - _frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - _frame.getHeight()) / 2);
+        _frame.setLocation(x, y);
 
-    return true;
-  }
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _frame.setSize(1000, 1000);
+
+
+        JPanel MainPanel = new JPanel();
+
+        _frame.add(MainPanel);
+
+        GridLayout experimentLayout = new GridLayout(10, 10);
+        MainPanel.setLayout(experimentLayout);
+
+        JPanel[][] squares = new JPanel[10][10];
+
+        int rowCounter = 0;
+        int rows = 0;
+
+        for (int i = 0; i < 100; i++) {
+
+            JPanel sub = new JPanel();
+//            sub.setBorder(BorderFactory.createLineBorder(Color.black));
+//            sub.setBackground(Color.red);
+            sub.setSize(100, 100);
+            sub.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    System.out.println("Click");
+                }
+            });
+
+
+            if (rowCounter >= 10) {
+                rowCounter = 0;
+                rows++;
+            }
+
+            System.out.println("Adding square at row " + rows + ", col " + (i % 10));
+            squares[rows][i % 10] = sub;
+
+
+            MainPanel.add(sub);
+
+            rowCounter++;
+        }
+
+        squares[1][1].setBackground(Color.green);
+
+        _frame.setVisible(true);
+
+        return true;
+    }
 
 }

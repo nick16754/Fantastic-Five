608b9c1075d9ec9b33c7b5c3cddb00bf7f78b8f5
bdietz56 <bradleydietz@Bradleys-MacBook-Pro.local>
2017-11-08 11:47:17 -0500

    Revert "Merge pull request #29 from nick16754/Deck-and-Display"
    This reverts commit 455cfd1d987565173eb8013cc64b4742f0ab92db, reversing
    changes made to c0796224e3656e1ae1351d643b2f408ecf2e8586.

--- src/main/java/WoSCard.java
+++ src/main/java/WoSCard.java
@@ -1,25 +1,25 @@
 public class WoSCard{
-  private boolean doubleCard; //does card move one or two spaces (false for special cards)
-  private String typeCard; //color or special (Skip Turn or Go To Middle)
+  private boolean oneOrTwo; //does card move one or two spaces
+  private String color; //color
 
-  public WoSCard(boolean number, String colorOrSpecial){
-    this.doubleCard = number;
-    this.typeCard = colorOrSpecial;
+  public WoSCard(boolean number, String whatColor){
+    oneOrTwo = number;
+    color = whatColor;
   }
 
-  public void setDoubleCard (boolean oneOrTwo){
-    this.doubleCard = oneOrTwo;
+  public void setDoubleCard (boolean number){
+    oneOrTwo = number;
   }
 
   public boolean getDoubleCard (){
-    return this.doubleCard;
+    return oneOrTwo;
   }
 
-  public void setCardType (String colorOrSpecial){
-    this.typeCard = colorOrSpecial;
+  public void setColor (String whatColor){
+    color = whatColor;
   }
 
-  public String getCardType (){
-    return this.typeCard;
+  public String getColor (){
+    return color;
   }
 }
